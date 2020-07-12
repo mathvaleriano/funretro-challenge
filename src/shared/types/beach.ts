5f18da7ce06b37f52d1a22d799c2a91ecd6ebd2a
Matheus Valeriano <math.valeriano@gmail.com>
2020-07-12 15:26:44 -0300

    feat: define type

---
+++ src/shared/types/beach.ts
@@ -0,0 +1,8 @@
+export type Beach = {
+  readonly id: string
+  readonly name: string
+  readonly image: string
+  readonly location: string
+  readonly state: string
+  readonly description: string
+}
