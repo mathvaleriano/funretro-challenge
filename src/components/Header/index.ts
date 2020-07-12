d42dde8dbdb74175b5cec182486ac1b75e7b08b4
Matheus Valeriano <math.valeriano@gmail.com>
2020-07-12 15:28:37 -0300

    feat: create header component

---
+++ src/components/Header/index.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+import Flex from '../Flex'
+
+export default styled(Flex)`
+  padding: 1rem;
+  position: fixed;
+  width: 100vw;
+  background-color: white;
+  box-shadow: 0px 1px 7px 1px rgba(0, 0, 0, 0.3);
+`
