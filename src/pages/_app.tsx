6f04a2d367d2b8d8a074706a766f1f552c577c61
Matheus Valeriano <math.valeriano@gmail.com>
2020-07-12 22:41:48 -0300

    fix: lighthouse specifications

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,36 +4,41 @@ import Head from 'next/head'
 import 'normalize.css'
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => (
-  <>
+  <html lang="pt-br" dir="ltr">
     <Head>
       <title>FunRetro Challenge</title>
       <link rel="icon" href="/favicon.ico" />
+      <link rel="preconnect" href="https://guiaviajarmelhor.com.br" />
+      <meta name="description" content="FunRetro Challenge by @mathvaleriano" />
+      <meta name="robots" content="noindex" />
     </Head>
 
-    <Component {...pageProps} />
+    <body>
+      <Component {...pageProps} />
 
-    <style jsx global>
-      {`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-          font-size: calc(0.75em + 1vmin);
-        }
+      <style jsx global>
+        {`
+          html,
+          body {
+            padding: 0;
+            margin: 0;
+            font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+              Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+              sans-serif;
+            font-size: calc(0.75em + 1vmin);
+          }
 
-        * {
-          box-sizing: border-box;
-        }
+          * {
+            box-sizing: border-box;
+          }
 
-        main {
-          padding-top: 12vh;
-        }
-      `}
-    </style>
-  </>
+          main {
+            padding-top: 12vh;
+          }
+        `}
+      </style>
+    </body>
+  </html>
 )
 
 export default memo(App)
