9cc20324b62f7bbec85deb9a93876dbeb46cc2f0
Matheus Valeriano <math.valeriano@gmail.com>
2020-07-16 16:03:08 -0300

    feat: improve performance
    use state instead request

--- src/shared/hooks/useBeaches.ts
+++ src/shared/hooks/useBeaches.ts
@@ -1,27 +1,78 @@
-import { useCallback, useState, useEffect } from 'react'
+import { useCallback, useEffect, useReducer } from 'react'
 import { Beach } from '../types/beach'
 import { getBeaches } from '../services/beaches'
 
+enum ACTION_TYPES {
+  SET_BEACHES,
+  FILTER_BEACHES,
+}
+
+type State = {
+  beaches: Beach[]
+  filteredBeaches: Beach[]
+}
+
+type Action =
+  | {
+      type: ACTION_TYPES.SET_BEACHES
+      payload: Beach[]
+    }
+  | {
+      type: ACTION_TYPES.FILTER_BEACHES
+      payload?: string
+    }
+
+const initialState: State = {
+  beaches: [],
+  filteredBeaches: [],
+}
+
+const reducer = (state: State, action: Action) => {
+  switch (action.type) {
+    case ACTION_TYPES.SET_BEACHES:
+      return {
+        ...state,
+        beaches: action.payload,
+        filteredBeaches: action.payload,
+      }
+    case ACTION_TYPES.FILTER_BEACHES:
+      return {
+        ...state,
+        filteredBeaches: action.payload
+          ? state.beaches.filter((beach) => beach.state === action.payload)
+          : state.beaches,
+      }
+    default:
+      return state
+  }
+}
+
 export default () => {
-  const [beaches, setBeaches] = useState<Beach[]>([])
+  const [state, dispatch] = useReducer(reducer, initialState)
 
   const handleFilter = useCallback(
-    (state = '') => {
-      getBeaches(state)
-        .then((data) => setBeaches(data))
-        .catch((error) => {
-          throw error
-        })
+    (state?: string) => {
+      dispatch({ type: ACTION_TYPES.FILTER_BEACHES, payload: state })
     },
-    [setBeaches]
+    [dispatch]
   )
 
   useEffect(() => {
-    handleFilter()
-  }, [handleFilter])
+    const handler = () => {
+      getBeaches()
+        .then((data) =>
+          dispatch({ type: ACTION_TYPES.SET_BEACHES, payload: data })
+        )
+        .catch((error) => {
+          throw error
+        })
+    }
+
+    handler()
+  }, [])
 
   return {
-    beaches,
+    beaches: state.filteredBeaches,
     handleFilter,
   }
 }
